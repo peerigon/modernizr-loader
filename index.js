459a7175b9c0b3d2ccf7e658928d84c61970bd65
Chris Gaudreau <chris@crystala.net>
2016-12-25 10:54:26 -0400

    Fix issue caused by not polluting global scope

--- index.js
+++ index.js
@@ -5,14 +5,18 @@ var modernizr = require("modernizr");
 function wrapOutput(output) {
     // Exposing Modernizr as a module.
     return ";(function(window){\n" +
-            output + "\n" +
-            "module.exports = window.Modernizr;\n" +
-            "})({});";
+           "var hadGlobal = 'Modernizr' in window;\n" +
+           "var oldGlobal = window.Modernizr;\n" +
+           output + "\n" +
+           "module.exports = window.Modernizr;\n" +
+           "if (hadGlobal) { window.Modernizr = oldGlobal; }\n" +
+           "else { delete window.Modernizr; }\n" +
+           "})(window);";
 }
 
 module.exports = function (config) {
     if (typeof this.cacheable === 'function') {
-        this.cacheable()
+        this.cacheable();
     }
 
     var cb = this.async();
